81e05113bc0083dabd75b2dac150f73cd8660217
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-10-12 21:23:56 +0300

    first master commit

---
+++ pr4.1.2/pr4.1.2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int N, j;
+	double S;
+	cout << "N = "; cin >> N;
+	S = 0;
+	j = 2;
+	while (j <= N)
+	{
+		S += j * (N - j) / pow(j, 2) + pow(N - j, 2);
+		j++;
+	}
+	cout << S << endl;
+	S = 0;
+	j = 2;
+	do {
+		S += j * (N - j) / pow(j, 2) + pow(N - j, 2);
+		j++;
+	} while (j <= N);
+	cout << S << endl;
+	S = 0;
+	for (j = 2; j <= N; j++)
+	{
+		S += j * (N - j) / pow(j, 2) + pow(N - j, 2);
+	}
+	cout << S << endl;
+	S = 0;
+	for (j = N; j >= 2; j--)
+	{
+		S += j * (N - j) / pow(j, 2) + pow(N - j, 2);
+	}
+	cout << S << endl;
+	return 0;
+}
